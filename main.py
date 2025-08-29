5bb731ca1b3b55b4275011286a1cd77e7c71de07
Defenestren <MarcialGodes@gmail.com>
2025-08-29 11:05:56 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,88 @@
+# Clase en vídeo: https://youtu.be/_y9qQZXE24A
+
+### Hola Mundo ###
+
+# Documentación oficial: https://fastapi.tiangolo.com/es/
+
+# Instala FastAPI: pip install "fastapi[all]"
+
+# https://fastapi.tiangolo.com/es/tutorial/first-steps/?h=primeros+pasos
+
+
+from fastapi import Cookie, FastAPI
+from routers import products, users, basic_auth_users, jwt_auth_users, users_db
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from typing import Annotated
+# import os
+
+app = FastAPI()
+
+
+# Routers
+app.include_router(products.router)
+app.include_router(users.router)
+app.include_router(basic_auth_users.router)
+app.include_router(jwt_auth_users.router)
+app.include_router(users_db.router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+# Url local: http://127.0.0.1:8000/static/images/lago.jpg
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/video", response_class=HTMLResponse)
+async def show_video():
+    return """
+    <html>
+        <head>
+            <title>Mi video</title>
+        </head>
+        <body style="display: flex; flex-direction: column; align-items: center; background-color: #111; color: white;">
+            <h1>🎥 Reproduciendo Whitesnake</h1>
+            <video id="myVideo" width="800" height="450" controls>
+                <source src="/static/videos/ll_ltpwhitesnake_1.mp4" type="video/mp4">
+                Tu navegador no soporta videos en HTML5.
+            </video>
+            <br>
+            <button onclick="openFullscreen()">Pantalla completa</button>
+
+            <script>
+                var video = document.getElementById("myVideo");
+                function openFullscreen() {
+                    if (video.requestFullscreen) {
+                        video.requestFullscreen();
+                    } else if (video.webkitRequestFullscreen) { // Safari
+                        video.webkitRequestFullscreen();
+                    } else if (video.msRequestFullscreen) { // IE11
+                        video.msRequestFullscreen();
+                    }
+                }
+            </script>
+        </body>
+    </html>
+    """
+
+
+@app.get("/")
+async def root():
+    return "¡Hola FastAPI!"
+# Url local: http://127.0.0.1:8000
+
+@app.get("/url")
+async def url():
+    return {"url_curso": "https://mouredev.com/python"}
+# Url local: http://127.0.0.1:8000/url
+
+
+@app.get("/items/")
+async def read_items(ads_id: Annotated[str | None, Cookie()] = None):
+    return {"ads_id": ads_id}
+
+
+# Inicia el server: uvicorn main:app --reload
+# Detener el server: CTRL+C
+
+# Documentación con Swagger: http://127.0.0.1:8000/docs
+# Documentación con Redocly: http://127.0.0.1:8000/redoc
